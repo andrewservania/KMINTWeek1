c2c71764262a4cde8e70a8f482d5b569c039b553
Andrew Servania <ajp.servania@outlook.com>
2015-09-03 15:24:26 +0200

    + Initial Commit

---
+++ SDLFramework/SDLFramework/Main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "Config.h"
+#include "FWApplication.h"
+#include <SDL_events.h>
+#include "SDL_timer.h"
+#include <time.h>
+
+int main(int args[])
+{
+	//auto window = Window::CreateSDLWindow();
+	auto application = new FWApplication();
+	if (!application->GetWindow())
+	{
+		LOG("Couldn't create window...");
+		return EXIT_FAILURE;
+	}
+	
+	application->SetTargetFPS(60);
+	application->SetColor(Color(255, 10, 40, 255));
+	
+
+	while (application->IsRunning())
+	{
+		application->StartTick();
+
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				application->Quit();
+				break;
+			case SDL_KEYDOWN:
+
+				switch (event.key.keysym.sym){
+
+				case SDLK_0:
+					
+					break;
+				default:
+					break;
+				}
+				break;
+			}
+		}
+		
+		application->SetColor(Color(0, 0, 0, 255));
+		application->DrawText("Welcome to KMint", 50, 50);
+		
+		// For the background
+		application->SetColor(Color(255, 255, 255, 255));
+
+		application->UpdateGameObjects();
+		application->RenderGameObjects();
+		application->EndTick();
+	}
+		
+	return EXIT_SUCCESS;
+}
